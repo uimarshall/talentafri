81a384b1e14876115f8ade08a6d2111a61248417
Marshall Akpan <uimarshall@gmail.com>
2025-04-22 07:15:55 +0100

    In this PR, I added user authentication for: - User registration - User login - JWT token generation

--- backend/src/controllers/userAuthController.js
+++ backend/src/controllers/userAuthController.js
@@ -2,6 +2,7 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/User.js';
 import ErrorHandler from '../utils/errorHandler.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc Register a new user
 // @route POST /api/v1/users/register
@@ -29,11 +30,52 @@ const registerUser = asyncHandler(async (req, res, next) => {
     location,
   });
 
-  return res.status(201).json({
-    success: true,
-    message: 'User registered successfully',
-    user: newUser,
-  });
+  // const token = newUser.getJwtToken();
+
+  // return res.status(201).json({
+  //   success: true,
+  //   message: 'User registered successfully',
+  //   user: newUser,
+  //   token,
+  // });
+  return generateToken(newUser, 201, res);
 });
 
-export default registerUser;
+// @desc: Login a user
+// @route: /api/v1/users/login
+// @access: protected
+
+const loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  // Check if email and password is entered in by user
+  if (!email || !password) {
+    next(new ErrorHandler('Please enter email and password', 400));
+    return;
+  }
+
+  // Find user in database
+  const userFound = await User.findOne({ email }).select('+password');
+  if (userFound == null) {
+    next(new ErrorHandler('Invalid email or password', 401));
+    return;
+  }
+
+  // Check if password is correct or not
+  const isPasswordMatched = await userFound.comparePassword(password);
+  if (!isPasswordMatched) {
+    next(new ErrorHandler('Invalid email or password', 401));
+    return;
+  }
+
+  // If everything is correct, then send json web token to user
+  // const token = userFound.getJwtToken();
+
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'User logged in successfully',
+  //   token,
+  // });
+  generateToken(userFound, 200, res);
+});
+
+export { registerUser, loginUser };
--- backend/src/routes/api/user.js
+++ backend/src/routes/api/user.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-import registerUser from '../../controllers/userAuthController.js';
+import { registerUser, loginUser } from '../../controllers/userAuthController.js';
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ const router = express.Router();
 // @route POST /api/v1/auth/register
 // @access Public
 router.post('/register', registerUser);
+// @desc Login a user
+router.post('/login', loginUser);
 
 export default router;
